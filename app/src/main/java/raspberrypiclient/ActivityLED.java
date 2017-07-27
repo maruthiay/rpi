d29b1a9250978579b3a9ef6766d1001c093d7e22
maruthiay <maruthiay@gmail.com>
2017-07-27 14:08:50 -0400

    "Updated login-registration"

---
+++ app/src/main/java/raspberrypiclient/ActivityLED.java
@@ -0,0 +1,101 @@
+package raspberrypiclient;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import linium.ledcontrol.R;
+
+
+public class ActivityLED extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainled);
+
+        /********************************/
+         /*    Define all the buttons    */
+        /********************************/
+        Switch led1 = (Switch) findViewById(R.id.led1);
+        Switch led2 = (Switch) findViewById(R.id.led2);
+        Switch led3 = (Switch) findViewById(R.id.led3);
+
+
+        /*******************************************************/
+         /*  Set an onclick/onchange listener for every button  */
+        /*******************************************************/
+
+        led1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    /* Switch is led 1 */
+                    new Background_get().execute("led1=1");
+                } else {
+                    new Background_get().execute("led1=0");
+                }
+            }
+        });
+
+        led2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    /* Switch is led 2 */
+                    new Background_get().execute("led2=1");
+                } else {
+                    new Background_get().execute("led2=0");
+                }
+            }
+        });
+
+        led3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    /* Switch is led 3 */
+                    new Background_get().execute("led3=1");
+                } else {
+                    new Background_get().execute("led3=0");
+                }
+            }
+        });
+    }
+
+    /*****************************************************/
+       /*  This is a background process for connecting      */
+      /*   to the arduino server and sending               */
+     /*    the GET request withe the added data           */
+    /*****************************************************/
+
+    private class Background_get extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                /* Change the IP to the IP you set in the arduino sketch */
+                URL url = new URL("http://192.168.20.10/?" + params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    result.append(inputLine).append("\n");
+
+                in.close();
+                connection.disconnect();
+                return result.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
--- app/src/main/java/raspberrypiclient/MainActivity.java
+++ app/src/main/java/raspberrypiclient/MainActivity.java
@@ -1,24 +1,15 @@
-package linium.ledcontrol;
+package raspberrypiclient;
 
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.ToggleButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import linium.ledcontrol.R;
 
 public class MainActivity extends Activity{
 
@@ -30,77 +21,22 @@ public class MainActivity extends Activity{
         /********************************/
          /*    Define all the buttons    */
         /********************************/
-        Switch led1 = (Switch) findViewById(R.id.led1);
-        Switch led2 = (Switch) findViewById(R.id.led2);
-        Switch led3 = (Switch) findViewById(R.id.led3);
+        final EditText username1 = (EditText) findViewById(R.id.username);
+        final EditText password1 = (EditText) findViewById(R.id.password);
+        final Button button1 = (Button) findViewById(R.id.button);
 
-
-        /*******************************************************/
-         /*  Set an onclick/onchange listener for every button  */
-        /*******************************************************/
-
-        led1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    /* Switch is led 1 */
-                    new Background_get().execute("led1=1");
+        button1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username1.getText().toString().equals("linium") && password1.getText().toString().equals("linium")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, ActivityLED.class);
+                        startActivity(intent);
                 } else {
-                    new Background_get().execute("led1=0");
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
-
-        led2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    /* Switch is led 2 */
-                    new Background_get().execute("led2=1");
-                } else {
-                    new Background_get().execute("led2=0");
-                }
-            }
-        });
-
-        led3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    /* Switch is led 3 */
-                    new Background_get().execute("led3=1");
-                } else {
-                    new Background_get().execute("led3=0");
-                }
-            }
-        });
-    }
-
-    /*****************************************************/
-       /*  This is a background process for connecting      */
-      /*   to the arduino server and sending               */
-     /*    the GET request withe the added data           */
-    /*****************************************************/
-
-    private class Background_get extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                /* Change the IP to the IP you set in the arduino sketch */
-                URL url = new URL("http://192.168.20.92/?" + params[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder result = new StringBuilder();
-                String inputLine;
-                while ((inputLine = in.readLine()) != null)
-                    result.append(inputLine).append("\n");
-
-                in.close();
-                connection.disconnect();
-                return result.toString();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
     }
 }
